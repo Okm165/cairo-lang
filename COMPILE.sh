fccd06b69c9fcfb6975008acf58412d85a8639f3
Bartosz Nowak <bartekn337@gmail.com>
2024-02-13 18:32:58 +0000

    misc

---
+++ COMPILE.sh
@@ -0,0 +1,13 @@
+cairo-compile --cairo_path=./src src/starkware/cairo/cairo_verifier/layouts/all_cairo/cairo_verifier.cairo --output cairo_verifier.json --no_debug_info --proof_mode
+
+cairo-run \
+    --program=cairo_verifier.json \
+    --layout=recursive_with_poseidon \
+    --program_input=cairo_verifier_input.json \
+    --air_public_input=cairo_verifier_public_input.json \
+    --air_private_input=cairo_verifier_private_input.json \
+    --trace_file=cairo_verifier_trace.json \
+    --memory_file=cairo_verifier_memory.json \
+    --print_output \
+    --proof_mode \
+    --print_info
--- README.md
+++ README.md
@@ -54,7 +54,7 @@ Once the docker image is built, you can fetch the python package zip file using:
 
 ```bash
 > container_id=$(docker create cairo)
-> docker cp ${container_id}:/app/cairo-lang-0.13.1.zip .
+> docker cp ${container_id}:/app/cairo-lang-0.13.1a0.zip .
 > docker rm -v ${container_id}
 ```
 
